b328b75f11eb4e8209f7bef54f379218f1afb3e5
LimpingGabriel <benjamin.nysetvold@gmail.com>
2023-02-05 01:39:15 -0500

    Added back some misc. files

--- FlightTracker/src/application/Main.java
+++ FlightTracker/src/application/Main.java
@@ -1,15 +1,10 @@
 package application;
 
 
-import application.map.MapWidget;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
  
 public class Main extends Application {
